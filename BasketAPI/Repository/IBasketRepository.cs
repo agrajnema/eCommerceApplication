f80cd06a81240b07884b85562093cf341f73eb2c
Agraj Nema <agraj.nema@tal.com.au>
2021-06-29 23:30:06 +1000

    Added basket api with redis cache db

---
+++ BasketAPI/Repository/IBasketRepository.cs
@@ -0,0 +1,15 @@
+﻿using BasketAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketAPI.Repository
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
