00df934a5427964d0c563abbde50fc1eb9b06f14
Agraj Nema <agraj.nema@tal.com.au>
2021-07-12 23:02:10 +1000

    Added Discount API with postgres db and pgadmin tool to manage postgres

---
+++ DiscountAPI/Program.cs
@@ -0,0 +1,29 @@
+using DiscountAPI.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscountAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           var host =  CreateHostBuilder(args).Build();
+            host.SeedDatabase<Program>();
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ DiscountAPI/Repositories/IDiscountRepository.cs
@@ -0,0 +1,17 @@
+﻿using DiscountAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiscountAPI.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
